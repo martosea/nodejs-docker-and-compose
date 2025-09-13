952f91d3279a2e7b2d2e4ddc9a6588104fe07882
Artem Everstov <everstovartem@yandex.ru>
2025-09-13 17:18:21 +0300

    frontend: use HTTPS API domain in production

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:3001";
+export const URL = "https://api.kpd.everstov.students.nomorepartiessbs.ru";
 
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
